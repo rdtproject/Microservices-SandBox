59999b51dd68d78a3a1ac57599a0e68a1ded91a1
Tpolm <rdt.project@gmail.com>
2020-12-05 12:34:05 +0100

    Added Jacoco code coverage report

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -14,21 +14,22 @@ pipeline {
             }
         }
         stage('Build') {
-            steps {                            
+            steps {
                 sh './mvnw clean package'
             }
 
-            post {                
+            post {
                 always {
-                    junit '**/target/surefire-reports/TEST-*.xml'                    
+                    junit '**/target/surefire-reports/TEST-*.xml'
                     emailext attachLog: true, body: 'Please go to ${BUILD_URL} and verify the build', compressLog: true, subject: "Job \'${JOB_NAME}\' (build ${BUILD_NUMBER}) ${currentBuild.result}", to: 'test@jenkins'
                 }
             }
         }
-        stage('Jacoco publish report')
+        stage('Jacoco publish report') {
             steps {
                 step( [ $class: 'JacocoPublisher' ] )
             }
+        }
         stage('SonarQube analysis') {
             steps {
                 // this groovy method comes from Sonar plugin installed in Jenkins (Manage Jenkins, configure system)
