20eafb6551b8d5eeebf7a34aee00f01959967f45
Tpolm <rdt.project@gmail.com>
2020-12-05 11:32:35 +0100

    Initial commit

---
+++ src/main/java/com/tpolm/microsandbox/MicrosandboxApplication.java
@@ -0,0 +1,13 @@
+package com.tpolm.microsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrosandboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicrosandboxApplication.class, args);
+	}
+
+}
