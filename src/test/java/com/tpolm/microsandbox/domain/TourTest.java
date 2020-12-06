7defb02fc0a03a7d004241753f723a0cd4639bb5
Tpolm <rdt.project@gmail.com>
2020-12-06 10:09:03 +0100

    Code smells

--- src/test/java/com/tpolm/microsandbox/domain/TourTest.java
+++ src/test/java/com/tpolm/microsandbox/domain/TourTest.java
@@ -8,22 +8,43 @@ import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
 class TourTest {
 
+    final static String TITLE = "Bratislavala tour";
+    final static String DESCRIPTION = "Visit Bratislava and have a lot of fun";
+    final static String BLURB = "Blurb";
+    final static Integer PRICE = 100;
+    final static String DURATION = "4 hours";
+    final static String BULLETS = "Some visiting bullets";
+    final static String KEYWORDS = "some keywords";
+    final static TourPackage TOUR_PACKAGE = new TourPackage("code", "name");
+    final static Difficulty DIFFICULTY = Difficulty.EASY;
+    final static Region REGION = Region.CENTRAL_COAST;
+
+
     @Test
     void createAndGetTour_test_if_constructor_works_fine() {
-        final String TITLE = "Bratislavala tour";
-        final String DESCRIPTION = "Visit Bratislava and have a lot of fun";
-        final String BLURB = "Blurb";
-        final Integer PRICE = 100;
-        final String DURATION = "4 hours";
-        final String BULLETS = "Some visiting bullets";
-        final String KEYWORDS = "some keywords";
-        final TourPackage TOUR_PACKAGE = new TourPackage("code", "name");
-        final Difficulty DIFFICULTY = Difficulty.EASY;
-        final Region REGION = Region.CENTRAL_COAST;
-
         Tour t = new Tour(TITLE, DESCRIPTION, BLURB, PRICE, DURATION, BULLETS,
                 KEYWORDS, TOUR_PACKAGE, DIFFICULTY, REGION);
 
+        assertIfAllAttributesAreCorrect(t);
+    }
+
+    @Test
+    void createAndGetTour_test_getters_and_setters() {
+        Tour t = new Tour();
+        t.setTitle(TITLE);
+        t.setDescription(DESCRIPTION);
+        t.setBlurb(BLURB);
+        t.setPrice(PRICE);
+        t.setDuration(DURATION);
+        t.setBullets(BULLETS);
+        t.setKeywords(KEYWORDS);
+        t.setTourPackage(TOUR_PACKAGE);
+        t.setRegion(REGION);
+
+        assertIfAllAttributesAreCorrect(t);
+    }
+
+    private void assertIfAllAttributesAreCorrect(Tour t) {
         assertEquals(TITLE, t.getTitle());
         assertEquals(DESCRIPTION, t.getDescription());
         assertEquals(BLURB, t.getBlurb());
@@ -34,5 +55,4 @@ class TourTest {
         assertEquals(TOUR_PACKAGE, t.getTourPackage());
         assertEquals(REGION, t.getRegion());
     }
-
 }
