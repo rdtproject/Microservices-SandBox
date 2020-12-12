2247e1f0f8729e93fb4e6ac85b0beb570c8a7938
rdtproject <1356awxrp>
2020-12-12 12:16:31 +0100

    Added logging

--- src/main/java/com/tpolm/microsandbox/service/TourRatingService.java
+++ src/main/java/com/tpolm/microsandbox/service/TourRatingService.java
@@ -4,20 +4,20 @@ import com.tpolm.microsandbox.domain.Tour;
 import com.tpolm.microsandbox.domain.TourRating;
 import com.tpolm.microsandbox.repository.TourRatingRepository;
 import com.tpolm.microsandbox.repository.TourRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.OptionalDouble;
+import java.util.*;
 
 @Service
 @Transactional
 public class TourRatingService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TourRatingService.class);
     private TourRatingRepository tourRatingRepository;
     private TourRepository tourRepository;
 
@@ -28,6 +28,7 @@ public class TourRatingService {
     }
 
     public void createNew(int tourId, Integer customerId, Integer score, String comment) throws NoSuchElementException {
+        LOGGER.info("Create Rating for tour {} of customers {}", tourId, customerId);
         tourRatingRepository.save(new TourRating(verifyTour(tourId), customerId,
                 score, comment));
     }
@@ -37,22 +38,25 @@ public class TourRatingService {
     }
 
     public List<TourRating> lookupAll()  {
+        LOGGER.info("Lookup all Ratings");
         return tourRatingRepository.findAll();
     }
 
     public Page<TourRating> lookupRatings(int tourId, Pageable pageable) throws NoSuchElementException  {
+        LOGGER.info("Lookup Rating for tour {}", tourId);
         return tourRatingRepository.findByTourId(verifyTour(tourId).getId(), pageable);
     }
 
     public TourRating update(int tourId, Integer customerId, Integer score, String comment) throws NoSuchElementException {
+        LOGGER.info("Update all of Rating for tour {} of customers {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         rating.setScore(score);
         rating.setComment(comment);
         return tourRatingRepository.save(rating);
     }
 
-    public TourRating updateSome(int tourId, Integer customerId, Integer score, String comment)
-            throws NoSuchElementException {
+    public TourRating updateSome(int tourId, Integer customerId, Integer score, String comment) throws NoSuchElementException {
+        LOGGER.info("Update some of Rating for tour {} of customers {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         if (score != null) {
             rating.setScore(score);
@@ -64,19 +68,23 @@ public class TourRatingService {
     }
 
     public void delete(int tourId, Integer customerId) throws NoSuchElementException {
+        LOGGER.info("Delete Rating for tour {} and customer {}", tourId, customerId);
         TourRating rating = verifyTourRating(tourId, customerId);
         tourRatingRepository.delete(rating);
     }
 
     public Double getAverageScore(int tourId)  throws NoSuchElementException  {
+        LOGGER.info("Get average score of tour {} by customers {}", tourId);
         List<TourRating> ratings = tourRatingRepository.findByTourId(verifyTour(tourId).getId());
         OptionalDouble average = ratings.stream().mapToInt((rating) -> rating.getScore()).average();
         return average.isPresent() ? average.getAsDouble():null;
     }
 
-    public void rateMany(int tourId,  int score, Integer [] customers) {
+    public void rateMany(int tourId,  int score, Integer[] customers) {
+        LOGGER.info("Rate tour {} by customers {}", tourId, Arrays.asList(customers).toString());
         tourRepository.findById(tourId).ifPresent(tour -> {
             for (Integer c : customers) {
+                LOGGER.debug("Attempt to create Tour Rating for customer {}", c);
                 tourRatingRepository.save(new TourRating(tour, c, score));
             }
         });
--- src/main/java/com/tpolm/microsandbox/web/TourRatingController.java
+++ src/main/java/com/tpolm/microsandbox/web/TourRatingController.java
@@ -2,6 +2,8 @@ package com.tpolm.microsandbox.web;
 
 import com.tpolm.microsandbox.domain.TourRating;
 import com.tpolm.microsandbox.service.TourRatingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +20,7 @@ import java.util.NoSuchElementException;
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(TourRatingController.class);
     private TourRatingService tourRatingService;
     private RatingAssembler ratingAssembler;
 
@@ -33,6 +36,7 @@ public class TourRatingController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public void createTourRating(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("POST /tours/{}/ratings", tourId);
         tourRatingService.createNew(tourId, ratingDto.getCustomerId(), ratingDto.getScore(), ratingDto.getComment());
     }
 
@@ -40,41 +44,47 @@ public class TourRatingController {
     @ResponseStatus(HttpStatus.CREATED)
     public void createManyTourRatings(@PathVariable(value = "tourId") int tourId,
                                       @PathVariable(value = "score") int score,
-                                      @RequestParam("customers") Integer customers[]) {
+                                      @RequestParam("customers") Integer[] customers) {
+        LOGGER.info("POST /tours/{}/ratings/{}", tourId, score);
         tourRatingService.rateMany(tourId, score, customers);
     }
 
     @GetMapping
     public PagedModel<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId, Pageable pageable,
                                                       PagedResourcesAssembler pagedAssembler) {
+        LOGGER.info("GET /tours/{}/ratings", tourId);
         Page<TourRating> tourRatingPage = tourRatingService.lookupRatings(tourId, pageable);
         return pagedAssembler.toModel(tourRatingPage, ratingAssembler);
     }
 
     @GetMapping("/average")
     public AbstractMap.SimpleEntry<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+        LOGGER.info("GET /tours/{}/ratings/average", tourId);
         return new AbstractMap.SimpleEntry<>("average", tourRatingService.getAverageScore(tourId));
     }
 
     @PutMapping
     public RatingDto updateWithPut(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PUT /tours/{}/ratings", tourId);
         return toDto(tourRatingService.update(tourId, ratingDto.getCustomerId(),
                 ratingDto.getScore(), ratingDto.getComment()));
     }
 
     @PatchMapping
     public RatingDto updateWithPatch(@PathVariable(value = "tourId") int tourId, @RequestBody @Validated RatingDto ratingDto) {
+        LOGGER.info("PATCH /tours/{}/ratings", tourId);
         return toDto(tourRatingService.updateSome(tourId, ratingDto.getCustomerId(),
                 ratingDto.getScore(), ratingDto.getComment()));
     }
 
     @DeleteMapping("/{customerId}")
     public void delete(@PathVariable(value = "tourId") int tourId, @PathVariable(value = "customerId") int customerId) {
+        LOGGER.info("DELETE /tours/{}/ratings/{}", tourId, customerId);
         tourRatingService.delete(tourId, customerId);
     }
 
     private RatingDto toDto(TourRating tourRating) {
-        return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getCustomerId());
+        return ratingAssembler.toModel(tourRating);
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
