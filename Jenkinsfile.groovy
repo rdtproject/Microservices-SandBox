db14441963af65a28b5e7740ab89449d494a07e0
rdtproject <1356awxrp>
2020-12-15 22:56:59 +0100

    Introduced Spring profiles

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,7 +15,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package'
+                sh './mvnw clean package -Dspring.profiles.active=mysql'
             }
 
             post {
