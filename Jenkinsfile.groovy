7b0b8e4153b8d75d7a2c78f07402ccaacc123b5c
rdtproject <1356awxrp>
2020-12-16 17:06:26 +0100

    Fixed Flyway vs H2

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -15,7 +15,7 @@ pipeline {
         }
         stage('Build') {
             steps {
-                sh './mvnw clean package -Dspring.profiles.active=mysql'
+                sh './mvnw clean package -Dspring.profiles.active=default'
             }
 
             post {
